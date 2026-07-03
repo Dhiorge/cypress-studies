8de478ffce1865f738c36530f1634c757ae1e3fb
Dhiorge Lopes <dhiorge.eng@gmail.com>
2026-07-03 05:37:26 -0400

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,3 @@
-/* 
-Exemplo dado pela ferramenta
-
-describe('template spec', () => {
-
-  it('passes', () => {
-
-    cy.viewport(1440,900)
-
-    cy.visit('http://localhost:3000')
-  })
-})
-
-*/
-//---------------------------------------------
-
 describe('Login', ()=>{
 
   it('Deve logar com sucesso', () => {
